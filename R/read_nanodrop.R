f2843e89050dc04412dddfb5297d384eef6f4461
Beatrice Bock <bmb646@nau.edu>
2024-05-14 15:06:56 -0700

    trying to fix issue with some files

--- R/read_nanodrop.R
+++ R/read_nanodrop.R
@@ -21,19 +21,31 @@ read_nanodrop <- function(filepath,
   # library(ggplot2)
   # library(dplyr)
 
-  tsv <- readr::read_tsv(filepath, col_names = F)
+  tsv <- readr::read_tsv(filepath, col_names = F, show_col_types = F)
 
   wvs <- length(seq(from = min_wv, to = max_wv, by = increments))
   row_x_sample <- wvs + junk_rows
-  nsamples <- nrow(tsv)/(wvs + junk_rows) #Works, round number
+  nsamples <- round(nrow(tsv)/(wvs + junk_rows)) #Works, round number
 
   ds <- data.frame()#empty df to store data
 
   for (i in 0:(nsamples-1)){
 
+    # junk_rows <- 10
+    #
+    # if (unname(unlist(lapply(strsplit(unlist(tsv[junk_rows+1+row_x_sample*i,1]), "\t"), `[[`, 1))) != "190.0") {
+    #
+    #   junk_rows = junk_rows -1
+    #
+    # }
     waves <- as.numeric(unname(unlist(lapply(strsplit(unlist(tsv[seq(junk_rows+1+row_x_sample*i, row_x_sample*(i+1)),1]), "\t"), `[[`, 1))))
-    abs <- as.numeric(unname(unlist(lapply(strsplit(unlist(tsv[seq(junk_rows+1+row_x_sample*i, row_x_sample*(i+1)),1]), "\t"), `[[`, 2))))
+    abs <- as.numeric(unname(unlist(lapply(strsplit(unlist(tsv[seq(junk_rows+1+row_x_sample*i, row_x_sample*(i+1)),1]), "\t"), `[[`, 2)))) #Something is messing up here
     samps <- as.character(unname(rep(tsv[1 + row_x_sample*i, 1], length(waves))))
+
+
+
+    head(as.numeric(unname(unlist(lapply(strsplit(unlist(tsv[seq(junk_rows+1+row_x_sample*i, row_x_sample*(i+1)),1]), "\t"), `[[`, 1)))))
+#Number 7 starts with 190.5 for some reason
 
     tog <- cbind(samps, waves, abs)
 
