3669bc1d56e78f2b18586f233798a70a894fdf7a
Beatrice Bock <bmb646@nau.edu>
2024-05-15 08:20:46 -0700

    fixed errors

--- R/read_nanodrop.R
+++ R/read_nanodrop.R
@@ -1,51 +1,47 @@
 #' read_nanodrop
 #'
-#' @param data dataset to use
+#' @param filepath the filepath to your tsv file
 #' @param max_wv max wavelength read by nanodrop
 #' @param min_wv min wavelength read by nanodrop
 #' @param increments wavelength increments set on machine
 #' @param junk_rows number of rows of metadata in the Nanodrop output
+#' @param filepath
 #'
 #' @return ds
 #' @export
 #'
-#' @examples read_nanodrop()
+#' @import readr
+#' @import stringr
+#'
+#' @examples read_nanodrop("C:/Users/beabo/OneDrive/Documents/NAU/R Package Dev/nanodRop/data/UV-Vis 5_14_2024 5_03_11 PM.tsv")
 read_nanodrop <- function(filepath,
-                          max_wv = 850,
-                          min_wv = 190,
+                          max_wv = "850.0",
+                          min_wv = "190.0",
                           increments = 0.5,
                           junk_rows = 10){
 
-  # library(readr)
-  # library(stringr)
-  # library(ggplot2)
-  # library(dplyr)
-
   tsv <- readr::read_tsv(filepath, col_names = F, show_col_types = F)
 
-  wvs <- length(seq(from = min_wv, to = max_wv, by = increments))
-  row_x_sample <- wvs + junk_rows
-  nsamples <- round(nrow(tsv)/(wvs + junk_rows)) #Works, round number
+  tsv <- stringr::str_split_fixed(as.matrix(tsv[,1]), n = 2, pattern = "\t")
+
+  start_rows <- which(tsv[,1]==min_wv)
+  end_rows <- which(tsv[,1]==max_wv)
+
+  nsamples <- length(start_rows)
 
   ds <- data.frame()#empty df to store data
 
-  for (i in 0:(nsamples-1)){
+  for (i in 1:nsamples){
 
-    # junk_rows <- 10
-    #
-    # if (unname(unlist(lapply(strsplit(unlist(tsv[junk_rows+1+row_x_sample*i,1]), "\t"), `[[`, 1))) != "190.0") {
-    #
-    #   junk_rows = junk_rows -1
-    #
-    # }
-    waves <- as.numeric(unname(unlist(lapply(strsplit(unlist(tsv[seq(junk_rows+1+row_x_sample*i, row_x_sample*(i+1)),1]), "\t"), `[[`, 1))))
-    abs <- as.numeric(unname(unlist(lapply(strsplit(unlist(tsv[seq(junk_rows+1+row_x_sample*i, row_x_sample*(i+1)),1]), "\t"), `[[`, 2)))) #Something is messing up here
-    samps <- as.character(unname(rep(tsv[1 + row_x_sample*i, 1], length(waves))))
+    waves <- tsv[start_rows[i]:end_rows[i], 1]
+    abs <- tsv[start_rows[i]:end_rows[i], 2]
 
-
-
-    head(as.numeric(unname(unlist(lapply(strsplit(unlist(tsv[seq(junk_rows+1+row_x_sample*i, row_x_sample*(i+1)),1]), "\t"), `[[`, 1)))))
-#Number 7 starts with 190.5 for some reason
+    if (i == 1){
+      samps <- rep(tsv[i, 1], length(waves))
+    }
+    else {
+      samps <- rep(tsv[end_rows[i-1]+1, 1], length(waves))
+    }
 
     tog <- cbind(samps, waves, abs)
 
