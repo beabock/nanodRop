2254c0db76bf65cdb6e3d777fedf6d9498c3bd20
Beatrice Bock <bmb646@nau.edu>
2024-05-24 11:01:19 -0700

    made it better

--- R/parse_waves.R
+++ R/parse_waves.R
@@ -6,7 +6,7 @@
 #' @param wv3 #Second peak of interest
 #' @param wv4 #Second valley to compare second peak to
 #'
-#' @return final_combined
+#' @return final_long
 #' @export
 #' @import tidyr
 #' @import dplyr
@@ -35,5 +35,18 @@ parse_waves <- function(dataset, wv1 = 223, wv2 = 270, wv3 = 542, wv4 = 644){
   # Combine the results
   final_combined <- purrr::reduce(result_list, dplyr::full_join, by = "samps")
 
-  return(final_combined)
+  final_long <- final_combined %>%
+    tidyr::pivot_longer(
+      cols = starts_with("diff_"),
+      names_to = "diff_type",
+      values_to = "diff"
+    )
+
+  final_long <- final_long %>%
+    dplyr::mutate(
+      nmdiff_542 = ifelse(diff_type == "diff_542_644", 1, 0),
+      ug_dye = 0.03467 + 0.02321 * nmdiff_542 + 0.00271 * diff
+    )
+
+  return(final_long)
 }
